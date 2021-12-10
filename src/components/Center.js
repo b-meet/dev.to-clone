b7228f913ef703b67f8ddfd633d99c275c89800b
Meet <meetbhalodiya1030@gmail.com>
2021-12-11 00:44:38 +0530

    make article skeleton html

--- src/components/Center.js
+++ src/components/Center.js
@@ -18,6 +18,32 @@ const Center = () => {
 					</ul>
 				</nav>
 			</header>
+			<main className='article-container'>
+				<article className='article-content-container'>
+					<header className='title-date-profile'>
+						<img src='' alt='profile' className='profile' />
+						<h4 className='title'></h4>
+						<p className='date'>
+							<time></time>
+						</p>
+					</header>
+					<main className='article-info'>
+						<h2 className='name-of-article'></h2>
+						<ul className='related-tags'>
+							<li></li>
+							<li></li>
+							<li></li>
+							<li></li>
+						</ul>
+					</main>
+					<footer className='interation-container'>
+						<p></p>
+						<p></p>
+						<time></time>
+						<p></p>
+					</footer>
+				</article>
+			</main>
 		</section>
 	);
 };
